4707e7aa623de98edcacde8ea9cf093a52070d82
BarbaraPSousa <barbarapsousa@hotmail.com>
2021-05-19 18:03:05 -0300

    Little improvement in board printing

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -6,7 +6,6 @@ package Chess;
 import Chess.pieces.King;
 import Chess.pieces.Rook;
 import boardgame.Board;
-import boardgame.Position;
 
 public class ChessMatch {
 	
@@ -27,11 +26,13 @@ public class ChessMatch {
 		}
 		return mat;		
 	}
+	private void placeNewPiece(char column, int row, ChessPiece piece) {//operação que passa a na cordenada do xadrez e não matriz
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
 	
-	private void initialStep() {//responsavel por iniciar a partida de xadrez e coloca a peça no tabuleiro
-		board.placePice(new Rook(board, Color.WHITE),new Position(2, 1));
-		board.placePice(new King(board, Color.BLACK),new Position(0, 4));
-		board.placePice(new King(board, Color.WHITE),new Position(7, 7));
-
+	private void initialStep() {//responsavel por iniciar a partida de xadrez e coloca a peça no tabuleiro do xadrez
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
